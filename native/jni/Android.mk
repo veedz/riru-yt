8a6bc4264a55310bc9f553827fde91925768aaf1
veedz <m.al.hafizd@gmail.com>
2022-12-30 22:40:24 +0700

    Update Android.mk

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := youtubervx
+LOCAL_MODULE := YTReVancedExt
 LOCAL_SRC_FILES := am_proc_start.cpp base.cpp cus.cpp
 LOCAL_STATIC_LIBRARIES := libcxx
 LOCAL_LDLIBS := -llog
